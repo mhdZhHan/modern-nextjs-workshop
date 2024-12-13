d454fc6a762a65297fdc028f779e5892db0efc95
Mohammed <dev.mohammedsh@gmail.com>
2024-12-13 11:15:51 +0530

    env config

---
+++ env.ts
@@ -0,0 +1,20 @@
+import { createEnv } from "@t3-oss/env-nextjs"
+import { z } from "zod"
+
+export const env = createEnv({
+  client: {
+    NEXT_PUBLIC_API_URL: z.string().url(),
+  },
+
+  server: {
+    DATABASE_URL: z
+      .string()
+      .url("DATABASE url not provided")
+      .regex(/postgres/),
+  },
+
+  runtimeEnv: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    DATABASE_URL: process.env.DATABASE_URL,
+  },
+})
