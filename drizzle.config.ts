e737859ebd2e8029f83ae3d43fb7b6fcb41d753d
Mohammed <dev.mohammedsh@gmail.com>
2024-12-14 11:56:51 +0530

    db generate & migrate

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "drizzle-kit"
 
 export default defineConfig({
   schema: "./src/db/schema.ts",
-  out: "./supabase/migrations",
+  out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
     url: env.DATABASE_URL,
