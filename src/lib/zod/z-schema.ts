e4a0a1a583c2e4de8f4381f23ff8d64547ae01a6
Mohammed <dev.mohammedsh@gmail.com>
2024-12-15 12:13:06 +0530

    💡

--- src/lib/zod/z-schema.ts
+++ src/lib/zod/z-schema.ts
@@ -1,22 +1,13 @@
 import { ZodType, z } from "zod"
 
-// export const zResponseSchema = <DataType extends z.ZodTypeAny>(
-//   dataType: DataType
-// ) =>
-//   z.object({
-//     success: z.boolean(),
-//     message: z.string(),
-//     data: dataType.optional(),
-//   });
-
-// export type ZResponse<T> = z.infer<ReturnType<typeof zResponseSchema<ZodType<T>>>>;
-
 export const zResponseSchema = <T>(dataType: z.ZodType<T>) =>
   z.object({
     success: z.boolean(),
     message: z.string(),
     data: dataType.optional(),
   })
+
+// export type ZResponse<T> = z.infer<ReturnType<typeof zResponseSchema<ZodType<T>>>>;
 
 export type ZResponse<T> = {
   success: boolean
