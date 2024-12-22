95713c82f052e462c726b3ebc7cf605815a274b5
Mohammed <dev.mohammedsh@gmail.com>
2024-12-22 12:42:33 +0530

    🧽 new protected routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
 
-const isProtectedRoute = createRouteMatcher(["/"])
+const isProtectedRoute = createRouteMatcher([
+  "/dashboard",
+  "/editor",
+  "/profile",
+])
 
 export const config = {
   matcher: [
