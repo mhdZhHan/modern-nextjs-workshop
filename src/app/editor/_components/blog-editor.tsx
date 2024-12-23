0a606086b176847ed4ca43c3e12cd43fd0548529
Mohammed <dev.mohammedsh@gmail.com>
2024-12-23 23:26:26 +0530

    🌶️ heder ui update

--- src/app/editor/_components/blog-editor.tsx
+++ src/app/editor/_components/blog-editor.tsx
@@ -2,59 +2,153 @@
 
 import { useState } from "react"
 import EditorNavBar from "./editor-navbar"
+import { useSession } from "@clerk/nextjs"
 
 import type { JSONContent } from "novel"
 import Editor from "@/components/editor/noval-editor"
+import PublishForm from "./publish-form"
+import { NewPost } from "@/db/schema"
+import { toast } from "@/hooks/use-toast"
 
+// Constants
+import { MAX_FILE_SIZE, ACCEPTED_IMAGE_TYPES } from "@/lib/constants"
+
+// lib
+import { createClerkSupabaseClient } from "@/lib/supabase/client"
 
 const BlogEditor = () => {
-  const [value, setValue] = useState<JSONContent>()
+  const [editorState, seEditorState] = useState<"editor" | "publish">("editor")
+  const [blogContent, setBlogContent] = useState<JSONContent>()
+  const [blogData, setBlogData] = useState<NewPost>({
+    title: "",
+    authorId: "",
+    banner: "",
+    categoryId: "",
+    content: "",
+    slug: "",
+    tagIds: [],
+  })
+  const [bannerPreview, setBannerPreview] = useState<string>(
+    "/blog-banner-dark.png"
+  )
 
+  const { session } = useSession()
+
+  // ANCHOR: Title Events
   const handleTitleKeyDown = (
     evt: React.KeyboardEvent<HTMLTextAreaElement>
   ) => {
-    // enter key
-    if (evt.keyCode === 13) {
+    if (evt.key === "Enter") {
       evt.preventDefault()
     }
   }
 
   const handleTitleChange = (evt: React.ChangeEvent<HTMLTextAreaElement>) => {
     const input = evt.target
+    input.style.height = "auto"
     input.style.height = input.scrollHeight + "px"
+
+    setBlogData((prev) => ({
+      ...prev,
+      title: input.value,
+    }))
+  }
+
+  // ANCHOR: Banner Upload
+  const handleBannerUpload = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const file = event.target.files?.[0]
+    if (!file) return
+
+    const supabase = createClerkSupabaseClient(session)
+
+    try {
+      const fileName = `banners/${Date.now()}_${file.name}`
+      const { data, error } = await supabase.storage
+        .from("inkspire_assets")
+        .upload(fileName, file)
+
+      if (error) {
+        throw error
+      }
+      console.log("File uploaded successfully:", data)
+
+      const {
+        data: { publicUrl },
+      } = supabase.storage.from("inkspire_assets").getPublicUrl(fileName)
+      console.log("Public URL:", publicUrl)
+    } catch (error) {
+      console.error("Error uploading banner:", error)
+    }
+  }
+
+  // ANCHOR: Verify publish
+  const verifyBlogPublish = () => {
+    if (!blogData.banner.length) {
+      return toast({
+        title: "Upload a blog banner to publish it",
+      })
+    }
+
+    if (!blogData.title.length) {
+      return toast({
+        title: "Write blog title to publish it",
+      })
+    }
+    seEditorState("publish")
   }
 
   return (
     <>
-      <EditorNavBar />
+      {editorState ? (
+        <>
+          <EditorNavBar handleBlogPublish={verifyBlogPublish} />
 
-      <div className="z-50 mx-auto w-full max-w-[900px] pb-8 pt-24">
-        <div className="border-grey aspect-video border-4 hover:opacity-80">
-          <label htmlFor="uploadBanner">
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img src="/blog-banner-dark.png" alt="blog-banner" />
+          <div className="z-50 mx-auto w-full max-w-[900px] pb-8 pt-24">
+            <div className="border-grey aspect-video border-4 hover:opacity-80">
+              <label htmlFor="uploadBanner">
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img src={bannerPreview} alt="blog-banner" />
+                <input
+                  type="file"
+                  id="uploadBanner"
+                  accept={ACCEPTED_IMAGE_TYPES.join(",")}
+                  hidden
+                  onChange={handleBannerUpload}
+                />
+              </label>
+            </div>
 
-            <input
-              type="file"
-              id="uploadBanner"
-              accept=".png, .jpg, .jpeg"
-              hidden
-              onChange={() => {}}
+            <textarea
+              placeholder="Blog Title"
+              value={blogData.title}
+              className="mt-10 h-20 w-full resize-none bg-inherit text-4xl font-medium leading-tight outline-none placeholder:opacity-40"
+              onKeyDown={handleTitleKeyDown}
+              onChange={handleTitleChange}
             />
-          </label>
-        </div>
 
-        <textarea
-          placeholder="Blog Title"
-          defaultValue=""
-          className="mt-10 h-20 w-full resize-none bg-inherit text-4xl font-medium leading-tight outline-none placeholder:opacity-40"
-          onKeyDown={handleTitleKeyDown}
-          onChange={handleTitleChange}
-        ></textarea>
-
-        <Editor initialValue={value} onChange={() => setValue} />
-      </div>
+            <Editor
+              initialValue={blogContent}
+              onChange={(newValue) => {
+                setBlogContent(newValue)
+                setBlogData((prev) => ({
+                  ...prev,
+                  content: newValue,
+                }))
+              }}
+            />
+          </div>
+        </>
+      ) : (
+        <PublishForm
+          seEditorState={seEditorState}
+          blogData={blogData}
+          setBlogData={setBlogData}
+        />
+      )}
     </>
   )
 }
+
 export default BlogEditor
--- src/app/editor/_components/editor-navbar.tsx
+++ src/app/editor/_components/editor-navbar.tsx
@@ -2,19 +2,25 @@ import Link from "next/link"
 import { ThemeToggle } from "../../../components/common/theme-toggle"
 import { Button } from "../../../components/ui/button"
 
-const EditorNavBar = () => {
+type EditorNavBarProps = {
+  handleBlogPublish: () => void
+}
+
+const EditorNavBar = ({ handleBlogPublish }: EditorNavBarProps) => {
   return (
-    <header className="fixed left-0 right-0 top-0 z-10 bg-background shadow-md">
+    <header className="fixed left-0 right-0 top-0 z-10 border-b bg-background shadow-md">
       <div className="container mx-auto flex flex-wrap items-center justify-between px-4 py-4">
-        <Link href="/" className="text-2xl font-bold">
-          Inkspire
+        <Link href="/blog" className="text-2xl font-bold">
+          Inkspire Modern Blog
         </Link>
         <span className="text-md">New blog</span>
 
         <nav className="mt-4 flex w-full items-center space-x-4 md:mt-0 md:w-auto">
-          <Button variant={"default"}>Publish</Button>
+          <Button variant={"default"} onClick={handleBlogPublish}>
+            Publish
+          </Button>
 
-          <Button variant={"outline"}>Draft</Button>
+          <Button variant={"outline"}>Save Draft</Button>
 
           <ThemeToggle />
         </nav>
