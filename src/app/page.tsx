3a0a1f7c674b49c15da907eae078475d35f8712e
Mohammed <dev.mohammedsh@gmail.com>
2025-01-04 11:12:22 +0530

    🧭 update home page & protected routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,31 +4,26 @@ import Image from "next/image"
 export default function Home() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+      <main className="row-start-2 flex flex-col items-center gap-6 sm:items-start">
+        <h1 className="bg-gradient-to-r from-green-400 via-emerald-500 to-teal-600 bg-clip-text text-5xl text-transparent">
+          Inkspire
+        </h1>
+
         <ol className="list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm sm:text-left">
           <li className="mb-2">
-            Get started by editing{" "}
+            Hey!, There,{" "}
             <code className="rounded bg-black/[.05] px-1 py-0.5 font-semibold dark:bg-white/[.06]">
-              src/app/page.tsx
+              Explore Stories & Share Ideas
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          A platform to read, write, and expand your perspective.
         </ol>
 
         <div className="flex flex-col items-center gap-4 sm:flex-row">
           <Link
             className="flex h-10 items-center justify-center gap-2 rounded-full border border-solid border-transparent bg-foreground px-4 text-sm text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] sm:h-12 sm:px-5 sm:text-base"
             href="/blog"
-            rel="noopener noreferrer"
           >
             <Image
               className="dark:invert"
@@ -37,14 +32,13 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Goto Blogs Page
+            Start reading
           </Link>
           <Link
             className="flex h-10 items-center justify-center rounded-full border border-solid border-black/[.08] px-4 text-sm transition-colors hover:border-transparent hover:bg-[#f2f2f2] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] sm:h-12 sm:min-w-44 sm:px-5 sm:text-base"
-            href="/editor"
-            rel="noopener noreferrer"
+            href="/write"
           >
-            Editor
+            Start Writing
           </Link>
         </div>
       </main>
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,8 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
 
 const isProtectedRoute = createRouteMatcher([
   "/dashboard(.*)",
-  "/editor",
-  "/profile",
+  "/write",
+  "/blog/bookmarks",
 ])
 
 export const config = {
